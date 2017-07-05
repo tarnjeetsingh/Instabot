dae82b30a0d293969a9ab5ba94ca93d929fa2d13
Tarnjeet Singh <jittarn@hotmail.com>
2017-07-05 20:27:59 +0530

    first commit

--- instabot.py
+++ instabot.py
@@ -2,10 +2,50 @@ import requests
 app_access_token = '1552927974.120ce8d.08ca68409d6745a7867e181ceea6bb00'
 base = 'https://api.instagram.com/v1/'
 
-def self_info:
+def self_info():
     request_url = (base+'users/self/?access_token=%s') % (app_access_token)
     print 'GET request url : %s' % (request_url)
     user_info = requests.get(request_url).json()
 
     if user_info['meta']['code']==200:
         if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'User does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+def get_user_id(insta_username):
+  request_url = (base+'users/search?q=%s&access_token=%s') % (insta_username, app_access_token)
+  print 'GET request url : %s' % (request_url)
+  user_info = requests.get(request_url).json()
+  if user_info['meta']['code'] == 200:
+      if len(user_info['data']):
+          return user_info['data'][0]['id']
+      else:
+          return None
+  else:
+      print 'Status code other than 200 received!'
+      exit()
+
+def get_user_info():
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (base+ 'users/%s?access_token=%s') % (user_id, app_access_token)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
